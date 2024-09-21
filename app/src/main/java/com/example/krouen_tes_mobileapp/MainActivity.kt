e15e484fc2f935debee06a23a4d64c5fc231e2f5
lao_thomorn <mornposeidon@gmail.com>
2024-09-21 19:07:26 +0700

    Initail Commit
    Signed-off-by: lao_thomorn <mornposeidon@gmail.com>

---
+++ app/src/main/java/com/example/krouen_tes_mobileapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.krouen_tes_mobileapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.krouen_tes_mobileapp.ui.theme.Krouen_Tes_MobileAPPTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Krouen_Tes_MobileAPPTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Krouen_Tes_MobileAPPTheme {
+        Greeting("Android")
+    }
+}
